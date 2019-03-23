7fdb565fbbef41c168ee37634e443f195728a700
sungbin <21700613@handong.edu>
2019-03-23 15:27:06 +0900

    bingo.c

---
+++ PA1/program/bingo.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <string.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+int select(); // files, protect, hide, status, exit, others(help)
+int main() {
+	int type;
+	char pid[50] = "";
+	type = select();
+	const char* PROC_PATH = "/proc/mexe";
+
+	switch(type) {
+	case 1: {
+		printf("1 is executed\n");
+		break;
+	}
+	case 2: {
+		printf("pid: ");
+		scanf("%s",pid);
+		FILE *fp = fopen(PROC_PATH, "w");
+		fputs(pid, fp);
+		fclose(fp);
+		break;
+	}
+	case 3: {
+		
+		break;
+	}
+	case 4: {
+
+		break;
+	}
+	case 5: {
+		printf("current protected pid: %s\n",pid);
+		break;
+	}
+	default: {
+	
+		break;
+	} }
+	
+	return 0;
+}
+
+int select() {
+	printf("Enter: ");
+	int type;
+	char in[128] = "";
+	scanf("%s",in);
+	if((strcmp(in,"fils")) == 0)
+		return 1;
+	else if((strcmp(in,"protect")) == 0)
+		return 2;
+	else if((strcmp(in,"hide")) == 0)
+		return 3;
+	else if((strcmp(in,"status")) == 0)
+		return 4;
+	else if((strcmp(in,"exit")) == 0)
+		return 5;
+	else
+		return -1;
+	return -1;
+}
