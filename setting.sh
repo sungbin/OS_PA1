94e1b8a87333019bbc433b139ba38c033c111112
sungbin <37574978+sungbin@users.noreply.github.com>
2019-03-28 23:37:32 +0900

    Update setting.sh

--- setting.sh
+++ setting.sh
@@ -3,3 +3,13 @@ apt-get install vim
 git config --global user.email "21700613@handong.edu"
 git config --global user.name "sungbin"
 git checkout sb
+
+sudo mkdir -p ~/4.15.0
+cd ~/4.15.0
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v4.15/linux-headers-4.15.0-041500_4.15.0-041500.201802011154_all.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v4.15/linux-headers-4.15.0-041500-generic_4.15.0-041500.201802011154_i386.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v4.15/linux-image-4.15.0-041500-generic_4.15.0-041500.201802011154_i386.deb
+dpkg -i *.deb
+sudo update-grub
+#reboot
+#uname -msr
